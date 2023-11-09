269d431aea3b67dddfd13a60c132fa55e7f89b3a
Piotr Szostak <pio.szo@proton.me>
2023-11-09 15:55:17 +0100

    Add docstring

--- palindrom.py
+++ palindrom.py
@@ -1,4 +1,7 @@
 def palindrom(arg):
+    """
+    Checks if a reversed string is eaqual to the string passed.
+    """
     rev = arg[::-1]
     return arg == rev
-print(palindrom('potop'))
+print(palindrom('a'))
