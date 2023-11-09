419703be40d8f49cd9cc701a40ef8082f041ba0a
Piotr Szostak <pio.szo@proton.me>
2023-11-09 16:02:15 +0100

    Add check for string

--- palindrom.py
+++ palindrom.py
@@ -1,7 +1,11 @@
 def palindrom(arg):
     """
-    Checks if a reversed string is eaqual to the string passed.
+    Checks if an argument is a string,
+    then it checks if a reversed string is eaqual to the string passed.
     """
-    rev = arg[::-1]
-    return arg == rev
-print(palindrom('a'))
+    if type(arg) == str:
+        rev = arg[::-1]
+        return arg == rev
+    else:
+        return False
+print(palindrom("121"))
